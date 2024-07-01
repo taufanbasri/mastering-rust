0da02a630984a64b4014618ba157a0e88bc1b962
Taufan Prasetyo <taufanremote@gmail.com>
2024-07-01 22:51:04 +0700

    Data Type Compound: Tuple

--- src/main.rs
+++ src/main.rs
@@ -125,3 +125,28 @@ fn char_type() {
 
     println!("{char1} {char2}")
 }
+
+#[test]
+fn tuple() {
+    // immutable tuple
+    // let data: (i32, f64, bool) = (10, 10.5, true);
+
+    // mutable tuple
+    let mut data: (i32, f64, bool) = (10, 10.5, true);
+
+    println!("{:?}", data);
+
+    // let a = data.0;
+    // let b = data.1;
+    // let c = data.2;
+
+    // destructure tuple
+    let (a, b, c) = data;
+    println!("{a} {b} {c}");
+
+    data.0 = 20;
+    data.1 = 20.5;
+    data.2 = false;
+
+    println!("{:?}", data);
+}
